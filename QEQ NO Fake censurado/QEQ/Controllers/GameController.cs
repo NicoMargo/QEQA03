5217f308cc03f5fe2f90560548fb5e20742bc775
NICOLAS ALEJANDRO MARGOSSIAN <43994080@ort>
2018-11-15 10:43:59 -0300

    view game JP

--- QEQ NO Fake censurado/QEQ/Controllers/GameController.cs
+++ QEQ NO Fake censurado/QEQ/Controllers/GameController.cs
@@ -33,8 +33,16 @@ namespace QEQ.Controllers
 
         public ActionResult JuegoPrincipalS()
         {
+            BD.CargarCats();
+            BD.CargarPersonajes();
+            ViewBag.Personajes = BD.Personajes;
             ViewBag.Preg = BD.Preguntas;
             return View();
+        }
+     
+        public ActionResult RiskS(int id)
+        {
+            return RedirectToAction("JuegoPrincipalS", "Game");
         }
 
         [HttpPost]
