ed3648dae54c8695d00f693f289cc8d5e6b36861
Zeky2002 <Zekykemeny@gmail.com>
2018-11-20 16:01:00 -0300

    puliendo los cambios de anush

--- QEQ NO Fake censurado/QEQ/Controllers/GameController.cs
+++ QEQ NO Fake censurado/QEQ/Controllers/GameController.cs
@@ -11,22 +11,8 @@ namespace QEQ.Controllers
     {
         //Funciones para el game single
         const int iRiskPenalty = 500;
-        public bool AskForOne(int idPer, int idPreg)
-        {
-            bool found = false;
-            int i = 0;
-            while (i < BD.Respuestas.Count() && !found)
-            {
-                if (BD.Respuestas[i].IdPersona == idPer && BD.Respuestas[i].IdPregunta == idPreg)
-                {
-                    found = true;
-                    BD.Respuestas.RemoveAt(i);
-                }
-                else { i++; }
-            }
-            return found;
-        }
-
+        
+        //Se ingresa un id, se devuelve la posicion en en la lista de la persona con esa id
         public static int BuscarPersonaje(int id)
         {
             bool found = false;
@@ -44,7 +30,7 @@ namespace QEQ.Controllers
             }
             return i;
         }
-
+        //Se ingresa un id, se devuleve la posicion en la lista de la pregunta con esa id
         public static int BuscarPregunta(int id)
         {
             Preg pregunta = null;
@@ -62,11 +48,26 @@ namespace QEQ.Controllers
             }
             return i;
         }
-
+        //Se ingresa un id de persona y un id de pregunta, devuelve true si existe una respuesta con esas dos id
+        public bool AskForOne(int idPer, int idPreg, bool Borrar)
+        {
+            bool found = false;
+            int i = 0;
+            while (i < BD.Respuestas.Count() && !found)
+            {
+                if (BD.Respuestas[i].IdPersona == idPer && BD.Respuestas[i].IdPregunta == idPreg)
+                {
+                    found = true;
+                    if(Borrar) BD.Respuestas.RemoveAt(i);
+                }
+                else { i++; }
+            }
+            return found;
+        }
         public Partida AskForAll(Partida lapartida, int idPreg) {
-            bool rtaElPersonaje = AskForOne(lapartida.Personaje1.Id,idPreg);
+            bool rtaElPersonaje = AskForOne(lapartida.Personaje1.Id,idPreg,false);
             for (int i = 0; i < BD.Personajes.Count(); i++) {
-                if (rtaElPersonaje != AskForOne(BD.Personajes[i].Id, idPreg)) {
+                if (rtaElPersonaje != AskForOne(BD.Personajes[i].Id, idPreg,true)) {
                     BD.Personajes.RemoveAt(i);
                 }
             }
@@ -111,29 +112,18 @@ namespace QEQ.Controllers
             ViewBag.Preg = BD.Preguntas;
             return View();
         }
+
         [HttpPost]
-        public ActionResult JuegoPrincipalS(int idpreg)
+        public ActionResult AskS(int idpreg)
         {
-            bool result = false;
-            int X = 0, While = -1;
-            if (BD.laPartida.Puntos > 500)
+            if (BD.laPartida.Puntos > iRiskPenalty)
             {
-                while (While == -1)
-                {
-                    if (BD.Preguntas[X].Id == idpreg)
-                    {
-                        result = AskS(idpreg);
-                        if (!result)
-                        {
-                            BD.Estado = 1;
-                        }
-                        BD.Preguntas.RemoveAt(X);
-                        X--;
-                        While = 1;
-                    }
-                    X++;
-                }
-                bool CorrectPer = false;
+                /*
+                bool result = false;
+                result = AskOne(idpreg);*/
+                Session["Estado"] = (BD.laPartida.Puntos <= iRiskPenalty);
+                bool CorrectPer = AskForOne(BD.laPartida.Personaje1.Id, idpreg,false);
+                /*
                 foreach (Rta resp in BD.Respuestas)
                 {
                     if (resp.IdPersona == BD.laPartida.Personaje1.Id && idpreg == resp.IdPregunta)
@@ -141,10 +131,12 @@ namespace QEQ.Controllers
                         CorrectPer = true;
                     }
                 }
+                */
                 for (int i = 0; i < BD.Personajes.Count; i++)
                 {
+                    /*
+                    bool correct = AskForOne(BD.Personajes[i].Id,idpreg);
                     int Z = 0;
-                    bool correct = false;
                     while (Z < BD.Respuestas.Count)
                     {
                         if (BD.Personajes[i].Id == BD.Respuestas[Z].IdPersona && BD.Respuestas[Z].IdPregunta == idpreg)
@@ -152,24 +144,24 @@ namespace QEQ.Controllers
                             correct = true;
                         }
                         Z++;
-                    }
-                    if ((correct == false && CorrectPer == true) || (correct == true && CorrectPer == false))
+                    }*/
+                    if (AskForOne(BD.Personajes[i].Id, idpreg,true) != CorrectPer)
                     {
-
-                        BD.Personajes.Remove(BD.Personajes[i]);
+                        //BD.Personajes.Remove(BD.Personajes[i]);
+                        BD.Personajes.RemoveAt(BuscarPersonaje(BD.Personajes[i].Id));
                         i--;
                     }
-                }
-                
-            }                      
+                }//Fin For que recorre los personajes
+                BD.laPartida.CantPreguntas++;
+                BD.laPartida.Puntos -= BD.BuscarPregunta(idpreg).Puntos;
+                BD.Preguntas.RemoveAt(BuscarPregunta(idpreg));
+            }//fin del if puntos < iRiskPenalty
             return RedirectToAction("JuegoPrincipalS", "Game");
         }
-
-        public static bool AskS(int idPreg) {
-           // BD.laPartida = AskForAll(BD.laPartida, idPreg);
+        /*
+        public static bool AskOne(int idPreg) {
             BD.laPartida.CantPreguntas++;
-            int puntos = BD.BuscarPregunta(idPreg).Puntos;
-            BD.laPartida.Puntos -= puntos;
+            BD.laPartida.Puntos -= BD.BuscarPregunta(idPreg).Puntos;
             if (BD.laPartida.Puntos <= iRiskPenalty)
             {
                 BD.laPartida.Finalizar(false);
@@ -180,16 +172,17 @@ namespace QEQ.Controllers
                 return true;
             }
         }
-        public ActionResult RiskS(int id)
+        */
+        public ActionResult RiskS(int idPersonaje)
         {
-            if (BD.laPartida.Personaje1.Id == id || BD.laPartida.Puntos<iRiskPenalty) {
-                BD.laPartida.Finalizar(!(BD.laPartida.Puntos < iRiskPenalty));
+            if (BD.laPartida.Personaje1.Id == idPersonaje || BD.laPartida.Puntos<iRiskPenalty) {
+                BD.laPartida.Finalizar(BD.laPartida.Personaje1.Id == idPersonaje);
                 return RedirectToAction("FinalizarS", "Game");
             }
             else
             {
                 BD.laPartida.Puntos -= iRiskPenalty;
-                BD.Personajes.RemoveAt(BuscarPersonaje(id));
+                BD.Personajes.RemoveAt(BuscarPersonaje(idPersonaje));
                 return RedirectToAction("JuegoPrincipalS", "Game");
             }
         }
